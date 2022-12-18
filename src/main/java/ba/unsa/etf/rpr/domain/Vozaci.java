ef2d176d4598e638b83a9bf63e230800d24b2b28
User <aliboudellaa150202@gmail.com>
2022-12-18 21:23:24 +0100

    getters and setters finished

--- src/main/java/ba/unsa/etf/rpr/domain/Vozaci.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Vozaci.java
@@ -14,4 +14,44 @@ public class Vozaci {
         this.adresa = adresa;
         this.datum = datum;
     }
+
+    public int getIdVozaca() {
+        return idVozaca;
+    }
+
+    public void setIdVozaca(int idVozaca) {
+        this.idVozaca = idVozaca;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getAdresa() {
+        return adresa;
+    }
+
+    public void setAdresa(String adresa) {
+        this.adresa = adresa;
+    }
+
+    public Date getDatum() {
+        return datum;
+    }
+
+    public void setDatum(Date datum) {
+        this.datum = datum;
+    }
 }
