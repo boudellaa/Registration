ed0e183bb871ff072515127bf9815d6bb750c6f0
User <aliboudellaa150202@gmail.com>
2022-12-29 22:57:42 +0100

    DaoFactory created and implemented

--- src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -1,24 +1,22 @@
 package ba.unsa.etf.rpr.dao;
 
 public class DaoFactory {
+    private static final VozilaDao vozilaDao = new VozilaDaoSQLImpl();
+    private static final VozaciDao vozaciDao = new VozaciDaoSQLImpl();
+    private static final RegistracijaDao registracijaDao = new RegistracijaDaoSQLImpl();
 
-    private static final RegistracijaDao registracijaDao = (RegistracijaDao) new RegistracijaDaoSQLImpl();
-    private static final VozaciDao vozaciDao = (VozaciDao) new VozaciDaoSQLImpl();
-    private static final VozilaDao vozilaDao = (VozilaDao) new VozilaDaoSQLImpl();
-
-    private DaoFactory(){
+    private DaoFactory() {
     }
 
-    public static RegistracijaDao registracijaDao(){
-        return registracijaDao;
-    }
-
-    public static VozaciDao vozaciDao(){
-        return vozaciDao;
-    }
 
     public static VozilaDao vozilaDao(){
         return vozilaDao;
+    }
+    public static VozaciDao vozaciDao(){
+        return vozaciDao;
+    }
+    public static RegistracijaDao registracijaDao(){
+        return registracijaDao;
     }
 
 }
