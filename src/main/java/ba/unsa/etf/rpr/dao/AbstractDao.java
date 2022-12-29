29bbe36e1378c7d13b6e551f749cf643e16bfb8d
User <aliboudellaa150202@gmail.com>
2022-12-29 06:30:08 +0100

    AbstractDao update

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -46,5 +46,16 @@ public abstract class AbstractDao<T extends Idable> implements Dao<T> {
         return executeQuery("SELECT * FROM "+ tableName, null);
     }
 
+    public void delete(int id) throws RegistrationException {
+        String sql = "DELETE FROM "+tableName+" WHERE id = ?";
+        try{
+            PreparedStatement stmt = getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            stmt.setObject(1, id);
+            stmt.executeUpdate();
+        }catch (SQLException e){
+            throw new RegistrationException(e.getMessage(), e);
+        }
+    }
+
 
 }
