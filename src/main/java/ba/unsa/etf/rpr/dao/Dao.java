472a05d399d9335f142f6f94e4e910095730b3d1
User <aliboudellaa150202@gmail.com>
2022-12-29 06:36:06 +0100

    Dao update

--- src/main/java/ba/unsa/etf/rpr/dao/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -1,4 +1,18 @@
 package ba.unsa.etf.rpr.dao;
 
-public interface Dao {
+import ba.unsa.etf.rpr.exceptions.RegistrationException;
+
+import java.util.List;
+
+public interface Dao<T> {
+
+    T getById(int id) throws RegistrationException;
+
+    T add(T t) throws RegistrationException;
+
+    T update(T t) throws RegistrationException;
+
+    void delete(int id) throws RegistrationException;
+
+    List<T> getAll() throws RegistrationException;
 }
