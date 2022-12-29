30a1238960539f59d1fcbcae0bfe06470d4f3405
User <aliboudellaa150202@gmail.com>
2022-12-29 06:17:16 +0100

    AbstractDao update

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -10,5 +10,20 @@ public abstract class AbstractDao<T extends Idable> implements Dao<T> {
     private Connection connection;
     private String tableName;
 
+    public AbstractDao(String tableName) {
+        try{
+            this.tableName = tableName;
+            Properties p = new Properties();
+            p.load(ClassLoader.getSystemResource("application.properties").openStream());
+            String url = p.getProperty("db.connection_string");
+            String username = p.getProperty("db.username");
+            String password = p.getProperty("db.password");
+            this.connection = DriverManager.getConnection(url, username, password);
+        }catch (Exception e){
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
 
 }
