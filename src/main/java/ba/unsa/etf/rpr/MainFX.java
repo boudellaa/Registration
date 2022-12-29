750996d543ee1db4070f4295942a12e5278ebfdb
User <aliboudellaa150202@gmail.com>
2022-12-29 23:01:08 +0100

    dependencies added

---
+++ src/main/java/ba/unsa/etf/rpr/MainFX.java
@@ -0,0 +1,23 @@
+package ba.unsa.etf.rpr;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class MainFX extends Application {
+        @Override
+        public void start(Stage primaryStage) throws Exception{
+            Parent root = FXMLLoader.load(getClass().getResource("/FXML/login.fxml"));
+            primaryStage.setTitle("hosa.exe");
+            primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        }
+        public static void main(String[] args){
+            launch(args);
+        }
+    }
