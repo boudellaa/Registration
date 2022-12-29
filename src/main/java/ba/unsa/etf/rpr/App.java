6d1c112d63d6c3136e3471c1f5cd424dc048c3b0
User <aliboudellaa150202@gmail.com>
2022-12-29 22:56:59 +0100

    App tested

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,13 +1,26 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.dao.*;
+import ba.unsa.etf.rpr.domain.Registracija;
+import ba.unsa.etf.rpr.domain.Vozaci;
+import ba.unsa.etf.rpr.domain.Vozila;
+import ba.unsa.etf.rpr.exceptions.RegistrationException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws RegistrationException {
     }
 }
