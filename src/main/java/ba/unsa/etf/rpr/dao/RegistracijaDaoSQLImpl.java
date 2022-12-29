0401d3311681eed9c6bedd223848e95196cc923d
User <aliboudellaa150202@gmail.com>
2022-12-29 22:58:34 +0100

    RegistracijaDaoSQLImpl updated

--- src/main/java/ba/unsa/etf/rpr/dao/RegistracijaDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RegistracijaDaoSQLImpl.java
@@ -1,4 +1,40 @@
 package ba.unsa.etf.rpr.dao;
 
-public class RegistracijaDaoSQLImpl {
+import ba.unsa.etf.rpr.domain.Registracija;
+import ba.unsa.etf.rpr.domain.Vozila;
+import ba.unsa.etf.rpr.exceptions.RegistrationException;
+
+import java.sql.ResultSet;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class RegistracijaDaoSQLImpl extends AbstractDao<Registracija> implements RegistracijaDao{
+
+    public RegistracijaDaoSQLImpl() {
+        super("Registracija");
+    }
+
+    @Override
+    public Registracija row2object(ResultSet rs) throws RegistrationException {
+        try {
+            Registracija r = new Registracija();
+            r.setId(rs.getInt("id"));
+            r.setDatum(rs.getDate("datum"));
+            r.setImeFirme(rs.getString("imeFirme"));
+            r.setVozilo(DaoFactory.vozilaDao().getById(rs.getInt("vozilo")));
+            return r;
+        } catch (Exception e) {
+            throw new RegistrationException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public Map<String, Object> object2row(Registracija object) {
+        Map<String, Object> m = new TreeMap<String, Object>();
+        m.put("id", object.getId());
+        m.put("datum", object.getDatum());
+        m.put("imeFirme", object.getImeFirme());
+        m.put("vozilo", object.getVozilo().getId());
+        return m;
+    }
 }
