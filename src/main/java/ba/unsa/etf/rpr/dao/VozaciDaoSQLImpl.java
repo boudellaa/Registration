cf1aa76f1f4af1ec2a71312c60e9ac5233a03633
User <aliboudellaa150202@gmail.com>
2022-12-29 22:59:40 +0100

    VozaciDaoSQLImpl updated

--- src/main/java/ba/unsa/etf/rpr/dao/VozaciDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/VozaciDaoSQLImpl.java
@@ -1,4 +1,43 @@
 package ba.unsa.etf.rpr.dao;
 
-public class VozaciDaoSQLImpl {
+import ba.unsa.etf.rpr.domain.Vozaci;
+import ba.unsa.etf.rpr.domain.Vozila;
+import ba.unsa.etf.rpr.exceptions.RegistrationException;
+
+import java.sql.ResultSet;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class VozaciDaoSQLImpl extends AbstractDao<Vozaci> implements VozaciDao{
+    public VozaciDaoSQLImpl() {
+        super("Vozaci");
+    }
+
+    @Override
+    public Vozaci row2object(ResultSet rs) throws RegistrationException {
+            try {
+                Vozaci v = new Vozaci();
+                v.setId(rs.getInt("id"));
+                v.setVozacIme(rs.getString("vozacIme"));
+                v.setVozacPrezime(rs.getString("vozacPrezime"));
+                v.setAdresa(rs.getString("adresa"));
+                v.setVozacEmail(rs.getString("vozacEmail"));
+                v.setVozacSifra(rs.getString("vozacSifra"));
+                return v;
+            } catch (Exception e) {
+                throw new RegistrationException(e.getMessage(), e);
+            }
+    }
+
+    @Override
+    public Map<String, Object> object2row(Vozaci object) {
+        Map<String, Object> m = new TreeMap<String, Object>();
+        m.put("id", object.getId());
+        m.put("vozacIme", object.getVozacIme());
+        m.put("vozacPrezime", object.getVozacPrezime());
+        m.put("adresa", object.getAdresa());
+        m.put("vozacEmail", object.getVozacEmail());
+        m.put("vozacSifra", object.getVozacSifra());
+        return m;
+    }
 }
