35e36f6a7d106479224136416fd3e78b6f52f16c
User <aliboudellaa150202@gmail.com>
2023-02-10 21:43:39 +0100

    implemention two new method

--- src/main/java/ba/unsa/etf/rpr/controllers/SignInController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/SignInController.java
@@ -1,23 +1,14 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class SignInController {
-    public Button signIn;
+    public Button btnCancel;
 
-    public void signInAction(ActionEvent actionEvent) throws IOException {
-        Stage stage = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource("/FXML/signIn.fxml"));
-        stage.setTitle("LOGIN");
-        stage.setScene(new Scene(root, 600, 400));
-        stage.setResizable(false);
-        stage.show();
+    public void actionClose(ActionEvent actionEvent) {
+        Stage stage = (Stage)btnCancel.getScene().getWindow();
+        stage.close();
     }
 }
