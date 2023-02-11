97596cbee1723155e94af3fce485728c63bd74dd
User <aliboudellaa150202@gmail.com>
2023-02-11 20:11:21 +0100

    some changes in SignInController

--- src/main/java/ba/unsa/etf/rpr/controllers/SignInController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/SignInController.java
@@ -2,13 +2,26 @@ package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class SignInController {
     public Button btnCancel;
+    public TextField usernameF;
+    public PasswordField passwordF;
+    public Button btnSgn;
 
     public void actionClose(ActionEvent actionEvent) {
         Stage stage = (Stage)btnCancel.getScene().getWindow();
         stage.close();
+    }
+
+    public void btnSgnAction(ActionEvent actionEvent) {
+        if(usernameF.getText().trim().isEmpty() || passwordF.getText().trim().isEmpty()){
+            AllertController.showAlert("Error", "Field blank", "Text fields must be filled in!");
+        }else if(usernameF.getText().trim().length() < 7 || usernameF.getText().trim().length() > 10){
+            AllertController.showAlert("Error", "Invalid username", "Username needs to be beetween 7 and 10 characters long!");
+        }
     }
 }
