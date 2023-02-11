4da8b47edbbf6ef3ba9c4ec9f92add8435e7d7ea
User <aliboudellaa150202@gmail.com>
2023-02-12 00:33:48 +0100

    Small changes in UserController

--- src/main/java/ba/unsa/etf/rpr/controllers/UserController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/UserController.java
@@ -1,16 +1,28 @@
 package ba.unsa.etf.rpr.controllers;
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.dao.VozilaDaoSQLImpl;
 import ba.unsa.etf.rpr.domain.Vozila;
 import ba.unsa.etf.rpr.exceptions.RegistrationException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 
-public class UserController implements Initializable {
-
+public class UserController {
+    public Button clsButton;
+    @FXML
+    private TableView table;
     @FXML
     private TableColumn<Vozila, String> bojaVozila;
 
@@ -20,8 +32,20 @@ public class UserController implements Initializable {
     @FXML
     private TableColumn<Vozila, String> imeVozila;
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    @FXML
+    public void initialize() throws RegistrationException {
+        bojaVozila.setCellValueFactory(new PropertyValueFactory<>("bojaVozila"));
+        imeVozila.setCellValueFactory(new PropertyValueFactory<>("imeVozila"));
+        cijenaVozila.setCellValueFactory(new PropertyValueFactory<>("cijenaVozila"));
+        List<Vozila> l = DaoFactory.vozilaDao().searchByVozac(SignInController.CurrentUser);
+        if(l == null) return;
+        ObservableList<Vozila> ob = FXCollections.observableArrayList();
+        ob.addAll(l);
+        table.setItems(ob);
+    }
 
+    public void clsButtonAction(ActionEvent actionEvent) {
+        Stage stage = (Stage)clsButton.getScene().getWindow();
+        stage.close();
     }
 }
