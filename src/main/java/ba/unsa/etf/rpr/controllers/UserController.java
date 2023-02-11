accc5e12ecd43bd469e773dbe207a7aee8e7830a
User <aliboudellaa150202@gmail.com>
2023-02-11 19:34:46 +0100

    changes in controller

--- src/main/java/ba/unsa/etf/rpr/controllers/UserController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/UserController.java
@@ -1,4 +1,26 @@
 package ba.unsa.etf.rpr.controllers;
+import ba.unsa.etf.rpr.domain.Vozila;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 
-public class UserController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class UserController implements Initializable {
+
+    @FXML
+    private TableColumn<Vozila, String> bojaVozila;
+
+    @FXML
+    private TableColumn<Vozila, Integer> cijenaVozila;
+
+    @FXML
+    private TableColumn<Vozila, String> imeVozila;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
