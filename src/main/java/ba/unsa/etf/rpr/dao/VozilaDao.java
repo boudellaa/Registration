bdd68c38e82db8e426a619ca1d278fe6ecdc0fd6
User <aliboudellaa150202@gmail.com>
2023-02-12 00:32:56 +0100

    VozilaDao updated

--- src/main/java/ba/unsa/etf/rpr/dao/VozilaDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/VozilaDao.java
@@ -1,11 +1,14 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.domain.Vozaci;
 import ba.unsa.etf.rpr.domain.Vozila;
 import ba.unsa.etf.rpr.exceptions.RegistrationException;
 
 import java.util.List;
 
 public interface VozilaDao extends Dao<Vozila>{
+
+    List<Vozila> searchByVozac(Vozaci v) throws RegistrationException;
 
     List<Vozila> searchByNameOfCar(String imeVozila) throws RegistrationException;
 
