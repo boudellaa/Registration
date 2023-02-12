d2f20395185ca3e1fdb013618c0c96a191c81580
User <aliboudellaa150202@gmail.com>
2023-02-12 17:02:21 +0100

    method implemented in VozilaManager

--- src/main/java/ba/unsa/etf/rpr/business/VozilaManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/VozilaManager.java
@@ -1,4 +1,15 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Vozila;
+import ba.unsa.etf.rpr.exceptions.RegistrationException;
+
+import java.util.List;
+
 public class VozilaManager {
+
+    public List<Vozila> getAll() throws RegistrationException {
+        return DaoFactory.vozilaDao().getAll();
+    }
+
 }
