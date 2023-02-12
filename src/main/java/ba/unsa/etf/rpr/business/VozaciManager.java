5d4e18bb7239ad4c3b3f169d38f587ef9d8c7379
User <aliboudellaa150202@gmail.com>
2023-02-12 17:09:47 +0100

    new class VozaciManager

---
+++ src/main/java/ba/unsa/etf/rpr/business/VozaciManager.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.rpr.business;
+
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Vozaci;
+import ba.unsa.etf.rpr.exceptions.RegistrationException;
+
+public class VozaciManager {
+
+}
