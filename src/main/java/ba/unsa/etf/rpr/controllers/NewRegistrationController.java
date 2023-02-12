70b33b995619a46912454c96907f3ce37000d538
User <aliboudellaa150202@gmail.com>
2023-02-12 12:08:23 +0100

    NewRegistrationController updated

--- src/main/java/ba/unsa/etf/rpr/controllers/NewRegistrationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/NewRegistrationController.java
@@ -5,10 +5,14 @@ import ba.unsa.etf.rpr.domain.Registracija;
 import ba.unsa.etf.rpr.domain.Vozila;
 import ba.unsa.etf.rpr.exceptions.RegistrationException;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Date;
 
@@ -26,7 +30,7 @@ public class NewRegistrationController {
     }
 
 
-    public void btnOkAction(ActionEvent actionEvent) throws RegistrationException {
+    public void btnOkAction(ActionEvent actionEvent) throws RegistrationException, IOException {
         if(fieldPrice.getText().trim().isEmpty() || fieldColor.getText().trim().isEmpty() || fieldName.getText().trim().isEmpty()){
             AllertController.showAlert("Error", "Field blank", "Text fields must be filled in!");
             return;
@@ -35,5 +39,13 @@ public class NewRegistrationController {
                 UserController.currentUser));
         LocalDate date = LocalDate.now();
         DaoFactory.registracijaDao().add(new Registracija(1, date, v));
+        Stage stage1 = (Stage)btnOK.getScene().getWindow();
+        stage1.close();
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/FXML/user.fxml"));
+        stage.setTitle("Your registred vehicles");
+        stage.setScene(new Scene(root, 600, 400));
+        stage.setResizable(false);
+        stage.show();
     }
 }
