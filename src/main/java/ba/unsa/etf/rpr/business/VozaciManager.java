7820e60200c71020f6eec13d3439458244c39acc
User <aliboudellaa150202@gmail.com>
2023-02-12 17:10:05 +0100

    new method in VozaciManager

--- src/main/java/ba/unsa/etf/rpr/business/VozaciManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/VozaciManager.java
@@ -5,5 +5,7 @@ import ba.unsa.etf.rpr.domain.Vozaci;
 import ba.unsa.etf.rpr.exceptions.RegistrationException;
 
 public class VozaciManager {
-
+    public Vozaci add(Vozaci v) throws RegistrationException {
+        return DaoFactory.vozaciDao().add(v);
+    }
 }
