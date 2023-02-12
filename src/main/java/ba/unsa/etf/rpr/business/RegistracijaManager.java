815cf765d15762eb76609d12dbb240b0ba6bb1ee
User <aliboudellaa150202@gmail.com>
2023-02-12 17:16:54 +0100

    new method implemented in RegistracijaManager

--- src/main/java/ba/unsa/etf/rpr/business/RegistracijaManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/RegistracijaManager.java
@@ -1,4 +1,14 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Registracija;
+import ba.unsa.etf.rpr.exceptions.RegistrationException;
+
+import java.util.List;
+
 public class RegistracijaManager {
+
+    public List<Registracija> getAll() throws RegistrationException {
+        return DaoFactory.registracijaDao().getAll();
+    }
 }
