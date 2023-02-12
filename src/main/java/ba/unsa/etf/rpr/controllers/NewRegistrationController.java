3688413c07e842c7d9da39c8328442f2bbe321ca
User <aliboudellaa150202@gmail.com>
2023-02-12 11:24:56 +0100

    new method in NewRegistrationController

--- src/main/java/ba/unsa/etf/rpr/controllers/NewRegistrationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/NewRegistrationController.java
@@ -1,5 +1,17 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class NewRegistrationController {
+
+    public Button cncBtn;
+
+    public void cncBtnAction(ActionEvent actionEvent) {
+        Stage stage = (Stage)cncBtn.getScene().getWindow();
+        stage.close();
+    }
+
 
 }
