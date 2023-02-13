57e1e2e4581c652129b9bad0770d9948ca5fd80e
User <aliboudellaa150202@gmail.com>
2023-02-13 16:10:56 +0100

    AllertController updated

--- src/main/java/ba/unsa/etf/rpr/controllers/AllertController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AllertController.java
@@ -2,6 +2,9 @@ package ba.unsa.etf.rpr.controllers;
 
 import javafx.scene.control.Alert;
 
+/**
+ * Controller class for showing allert
+ */
 public class AllertController {
     public static void showAlert(String title, String header, String content) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
