cde508f970d4fb805bb2c14b5fa77eff9580bca3
User <aliboudellaa150202@gmail.com>
2023-02-14 17:35:40 +0100

    RegistracijaManager updated

--- src/main/java/ba/unsa/etf/rpr/business/RegistracijaManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/RegistracijaManager.java
@@ -2,6 +2,7 @@ package ba.unsa.etf.rpr.business;
 
 import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Registracija;
+import ba.unsa.etf.rpr.domain.Vozaci;
 import ba.unsa.etf.rpr.exceptions.RegistrationException;
 
 import java.util.List;
@@ -22,6 +23,10 @@ public class RegistracijaManager {
 
     public void delete(int id) throws RegistrationException {
         DaoFactory.registracijaDao().delete(id);
+    }
+
+    public Registracija update(Registracija r) throws RegistrationException {
+        return DaoFactory.registracijaDao().update(r);
     }
 
     public Registracija getById(int id) throws RegistrationException {
